38cb7167b31eb41d1cff6f4cf77dc52369061116
unknown <vvlcek07@gmail.com>
2026-08-26 21:11:41 +0200

    Updates

---
+++ _next/static/2w_qBXjGezqT8-_mtnWpE/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/turisticky-denik//_next/:path+",
+        "destination": "/turisticky-denik/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
